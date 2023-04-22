07b80e45b79d6e8f403f10012dc7633cb8882bae
Divyao <94785328+Divyao@users.noreply.github.com>
2023-04-22 10:41:15 +0530

    Modified MacroNutri

--- src/components/macros.js
+++ src/components/macros.js
@@ -16,20 +16,20 @@ const Macros = (props) => {
             </Typography>
             <Typography variant="h6">
               <br />
-              calorie: {macroprop.calorie}
+              calorie: {macroprop?.calorie}
               </Typography>
-              {/* <Typography variant='h5'  gutterBottom>Balanced</Typography>
-              <Typography variant="h6">protein: {macroprop.balanced.protein} </Typography>
-              <Typography variant="h6">fat:  {macroprop.balanced.fat} </Typography>
-              <Typography variant="h6">carbs:  {macroprop.balanced.carbs} </Typography>
+              <Typography variant='h5'  gutterBottom>Balanced</Typography>
+              <Typography variant="h6">protein: {macroprop?.balanced?.protein} </Typography>
+              <Typography variant="h6">fat:  {macroprop?.balanced?.fat} </Typography>
+              <Typography variant="h6">carbs:  {macroprop?.balanced?.carbs} </Typography>
               <Typography variant='h5'  gutterBottom>lowfat</Typography>
-              <Typography variant="h6">protein: {macroprop.lowfat.protein} </Typography>
-              <Typography variant="h6">fat:  {macroprop.lowfat.fat} </Typography>
-              <Typography variant="h6">carbs:  {macroprop.lowfat.carbs} </Typography>
+              <Typography variant="h6">protein: {macroprop?.lowfat?.protein} </Typography>
+              <Typography variant="h6">fat:  {macroprop?.lowfat?.fat} </Typography>
+              <Typography variant="h6">carbs:  {macroprop?.lowfat?.carbs} </Typography>
               <Typography variant='h5'  gutterBottom>lowcarbs</Typography>
-              <Typography variant="h6">protein: {macroprop.lowcarbs.protein} </Typography>
-              <Typography variant="h6">fat:  {macroprop.lowcarbs.fat} </Typography>
-              <Typography variant="h6">carbs:  {macroprop.lowcarbs.carbs} </Typography> */}
+              <Typography variant="h6">protein: {macroprop?.lowcarbs?.protein} </Typography>
+              <Typography variant="h6">fat:  {macroprop?.lowcarbs?.fat} </Typography>
+              <Typography variant="h6">carbs:  {macroprop?.lowcarbs?.carbs} </Typography>
             </CardContent>
           <CardActions>
           </CardActions>
